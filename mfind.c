fb82cf5cbc51700cbc6097ebaa2d4cb21557a265
Sarah <sarahhale95@gmail.com>
2018-10-22 08:59:34 +0200

    woking on it

--- mfind.c
+++ mfind.c
@@ -15,13 +15,14 @@ void check_file_type(char * path){
     struct stat file_info;
 
     if(lstat(path, &file_info)==-1){
+         //free(path);
         fprintf(stderr,"no such %s : ", path);
         perror("");
     }else{ 
         if(S_ISDIR(file_info.st_mode) && !S_ISLNK(file_info.st_mode)){
             enqueue(concat_path(path, "/"));
-            //free(path);
-          
+        }else{
+            free(path);
         }
     }
 }
